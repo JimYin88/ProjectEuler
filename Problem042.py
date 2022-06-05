e66c590193ac81d7893e4d4044a6991bd026b46d
Jim Yin <jimyin88@gmail.com>
2022-06-05 06:45:13 -0400

    Improved version

--- Problem042.py
+++ Problem042.py
@@ -1,18 +1,22 @@
 '''
 Created on Jun 3, 2022
 
-@author: JimYi
+@author: Jim Yin
 '''
 
 import time
-
-time_start = time.time()
+time_start = time.perf_counter()
 
 words = []
-for word in open("p042_words.txt").read().strip().replace('"','').split(','):
-    words.append(word)
+# for word in open("p042_words.txt").read().strip().replace('"','').split(','):
+#     words.append(word)
 
-letter_map = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+with open("p042_words.txt") as f:
+    line = f.read()
+    words = line.strip().replace('"','').split(',')
+
+letter_map = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, \
+              'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
 
 def word_score(a_string):
     return sum(letter_map[letter] for letter in a_string)
@@ -25,9 +29,9 @@ print(sum(1 for w in words if word_score(w) in triangle_numbers))
 162
 '''
 
-time_end = time.time()
-print("time taken", time_end-time_start)
+time_end = time.perf_counter()
+print(f"time taken = {time_end-time_start} sec")
 
 '''
-time taken 0.003000020980834961
+time taken = 0.0031686000000000075 sec
 '''
--- Problem067.py
+++ Problem067.py
@@ -4,9 +4,13 @@ Created on Jun 2, 2022
 @author: Jim Yin
 '''
 
+import time
+time_start = time.perf_counter()
+
 lines = []
-for line in open("p067_triangle.txt").read().strip().split('\n'):
-    lines.append(list(map(int, line.split(" "))))
+with open("p067_triangle.txt") as f:
+    for line in f.read().strip().split('\n'):
+        lines.append(list(map(int, line.split(" "))))
 
 path_pre = lines[0]
 
@@ -18,7 +22,8 @@ for i in range(2, 101):
         elif j == i-1:
             path_next.append(int(lines[i-1][j])+int(path_pre[j-1]))
         else:
-            path_next.append(max(int(lines[i-1][j])+int(path_pre[j]), int(lines[i-1][j])+int(path_pre[j-1])))
+            path_next.append(max(int(lines[i-1][j])+int(path_pre[j]), \
+                                 int(lines[i-1][j])+int(path_pre[j-1])))
             
     path_pre = path_next
 
@@ -26,4 +31,12 @@ print(max(path_pre))
 
 '''
 7273
+'''
+
+time_end = time.perf_counter()
+print(f'time taken = {time_end - time_start} sec')
+
+
+'''
+time taken = 0.0089331 sec
 '''
