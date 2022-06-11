bf44e287f90957bb1c26c04ed1238ee12b3cc374
Jim Yin <jimyin88@gmail.com>
2022-06-11 06:41:35 -0400

    Improved version

--- Problem015.py
+++ Problem015.py
@@ -1,39 +1,24 @@
 # Created on Jun 10, 2022
 #
-# @author: JimYi
+# @author: Jim Yin
 
+import time
+start_time = time.perf_counter()
 
+layer = 20
 path_init = {(0, 0): 1}
 path_pre = path_init
 path_next = []
- 
 
-for i in range(20):
-    
-    for x, y in path_pre:
-        path_next.append([(x+1, y), path_pre[(x,y)]])
-        path_next.append([(x, y+1), path_pre[(x,y)]])
-    
-    path_next_dict = {}
-    for j in path_next:
-        if j[0] not in path_next_dict:
-            path_next_dict[j[0]] = j[1]
-        else:
-            path_next_dict[j[0]] += j[1]
-
-    path_pre = path_next_dict
-    path_next = [] 
-
-
-for i in range(20):
-    
+for i in range(layer*2):
     for x, y in path_pre:
         if x+1 <= 20:
             path_next.append([(x+1, y), path_pre[(x,y)]])
         if y+1 <= 20:
             path_next.append([(x, y+1), path_pre[(x,y)]])
-    
+
     path_next_dict = {}
+
     for j in path_next:
         if j[0] not in path_next_dict:
             path_next_dict[j[0]] = j[1]
@@ -41,8 +26,12 @@ for i in range(20):
             path_next_dict[j[0]] += j[1]
 
     path_pre = path_next_dict
-    path_next = [] 
+    path_next = []
 
-    
 print(path_pre[(20, 20)])
+end_time = time.perf_counter()
+print(f'Time taken = {end_time - start_time} sec')
 
+
+# 137846528820
+# Time taken = 0.0008680000000000077 sec
--- Problem040.py
+++ Problem040.py
@@ -2,6 +2,8 @@
 #
 # @author: Jim Yin
 
+import time
+
 
 def dnum(digit):
     dec_num_str = ''
@@ -15,19 +17,20 @@ def dnum(digit):
     return dec_num_str
 
 
-def main():
+def prob_040():
     d = 1000000
     print(int(dnum(d)[0])*int(dnum(d)[9])*int(dnum(d)[99])*int(dnum(d)[999])*int(dnum(d)[9999])*int(dnum(d)[99999]))
 
 
 if __name__ == '__main__':
-    main()
+    start_time = time.perf_counter()
+    prob_040()
+    end_time = time.perf_counter()
+    print(f'Time taken = {end_time - start_time} sec')
 
 
-'''
-210
-'''
-
+# 210
+# Time taken = 0.8030874 sec
 
 
 
