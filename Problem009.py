12a0d2b42fb5fe7ea7e57318abd64faedd4d49ba
Jim Yin <jimyin88@gmail.com>
2022-06-17 09:45:26 -0400

    Cleaned up the codes

--- Problem009.py
+++ Problem009.py
@@ -1,27 +1,31 @@
-'''
-Created on Jan 27, 2019
-
-@author: Jim Yin
-'''
+# Created on Jan 27, 2019
+#
+# @author: Jim Yin
 
 
-'''
-A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-a**2 + b**2 = c**2
-For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
-There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.
-'''
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a**2 + b**2 = c**2
+# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
 
-def triplet():
+import time
+
+
+def prob_009():
     for i in range(1,500):
         for j in range(i,500):
             k = (i*i + j*j)**0.5
             if i + j + k == 1000:
                 return int(i*j*k)
         
-print(triplet())
 
-'''
-31875000
-'''
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    print(prob_009())
+    end_time = time.perf_counter()
+    print(f'Time taken = {end_time - start_time} sec')
+
+
+# 31875000
+# Time taken = 0.037695599999999996 sec
