4e1e2429f302e1e400fc898da6b40f79cc5766a1
Jim Yin <jimyin88@gmail.com>
2022-06-18 14:40:22 -0400

    Clean up the codes

--- Problem020.py
+++ Problem020.py
@@ -1,17 +1,14 @@
-'''
-Created on May 25, 2022
+# Created on May 25, 2022
+#
+# @author: Jim Yin
 
-@author: Jim Yin
-'''
 
-'''
-n! means n × (n − 1) × ... × 3 × 2 × 1
-
-For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
-and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
-
-Find the sum of the digits in the number 100!
-'''
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
 
 import math
 import time
@@ -21,15 +18,15 @@ def count_digit(n):
     return sum(int(d) for d in str(n))
 
 
-def main():
-    start_time = time.perf_counter()
-    print(count_digit(math.factorial(100)))
-    end_time = time.perf_counter()
-    print(f'Time taken = {end_time - start_time} sec')
+def prob_020(n):
+    return count_digit(n)
 
 
 if __name__ == '__main__':
-    main()
+    start_time = time.perf_counter()
+    print(prob_020(math.factorial(100)))
+    end_time = time.perf_counter()
+    print(f'Time taken = {end_time - start_time} sec')
 
 
 # 648
