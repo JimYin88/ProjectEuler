4d4e3753b8069f5fa530dfee893bb2775d49dafd
Jim Yin <jimyin88@gmail.com>
2022-07-20 15:22:10 -0400

    Pep8 recoding

--- Problem058.py
+++ Problem058.py
@@ -1,60 +1,44 @@
-'''
-Created on Jun 3, 2022
+# Created on Jun 3, 2022
+#
+# @author: Jim Yin
 
-@author: Jim Yin
-'''
-
-import math
 import time
+from sympy import isprime
 
-time_start = time.perf_counter()
-
-def is_prime(n):
-    if n == 1:
-        return False 
-    elif n == 2 or n == 3:
-        return True
-    elif n % 2 == 0:
-        return False
-    else:
-        for i in range(3, int(math.sqrt(n))+1, 2):
-            if n%i==0:
-                return False
-    return True
 
 def find_spiral_primes():
     layer = 3
     number_prime = 0
     number_nonprime = 1
-    
+
     while True:
         lower_right = layer**2
         lower_left = layer**2 - layer + 1
         upper_left = layer**2 - 2*layer + 2
         upper_right = layer**2 - 3*layer + 3
-        
+
         diagonal = [lower_right, lower_left, upper_left, upper_right]
-        
+
         for n in diagonal:
-            if is_prime(n):
+            if isprime(n):
                 number_prime += 1
             else:
                 number_nonprime += 1
-        
+
         if number_prime/(number_prime + number_nonprime) < 0.1:
             return layer
         else:
             layer += 2
-        
-print(find_spiral_primes())
+
+
+if __name__ == '__main__':
+    time_start = time.perf_counter()
+    print(find_spiral_primes())
+    time_end = time.perf_counter()
+    print(f"time taken = {time_end - time_start} sec")
+
 
 '''
 26241
-'''
-
-time_end = time.perf_counter()
-print(f"time taken = {time_end - time_start} sec")
-
-'''
-time taken = 4.5867345 sec
+time taken = 0.2502003999999999 sec
 '''
