b0938be907276a1f2b900904193a42d08ff7ec66
Jim Yin <jimyin88@gmail.com>
2025-04-26 17:31:35 -0400

    Faster implementation

--- Problem092.py
+++ Problem092.py
@@ -4,30 +4,31 @@
 
 import time
 
-square_digit_chain_dict = {}
+
+square_digit_chain_dict = {1: 1, 89: 89}
+
 
 def square_digit_chain(n):
-    d = sum(int(d)**2 for d in str(n))
+    d = sum(int(d) ** 2 for d in str(n))
     if d in square_digit_chain_dict:
-        return d, square_digit_chain_dict[d]
+        return square_digit_chain_dict[d]
     nn = d
     while nn != 1 and nn != 89:
-        nn = sum(int(x)**2 for x in str(nn))
+        nn = sum(int(x) ** 2 for x in str(nn))
     square_digit_chain_dict[d] = nn
-    return d, nn
+    return nn
 
 
-def main():
-    print(sum(1 for i in range(1, 10**7) if square_digit_chain(i)[1] == 89))
+def prob_092(n):
+    return sum(1 for i in range(1, n) if square_digit_chain(i) == 89)
 
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    main()
+    print(prob_092(n=10**7))
     end_time = time.perf_counter()
     print(f"Time taken = {round(end_time - start_time, 2)} sec")
-    
+
 
 # 8581146
-# Time taken = 36.74 sec
-
+# Time taken = 22.8 sec
