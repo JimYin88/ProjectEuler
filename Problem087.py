86a1ce7e1366f310b73d7ea5498a727762f15afc
Jim Yin <jimyin88@gmail.com>
2025-04-26 17:12:37 -0400

    Faster implementation

--- Problem087.py
+++ Problem087.py
@@ -1,65 +1,38 @@
-# Created on Jun 3, 2022
+# Created on March 22, 2025
 #
 # @author: Jim Yin
 
-import math
+
 import time
-
-time_start = time.perf_counter()
-
-
-def prime_gen(lowerlimit, upperlimit):
-    num = max(2, lowerlimit)
-    if num == 2:
-        yield num
-        num += 1
-
-    if num == 3:
-        yield num
-        num += 2
-        
-    if num % 2 == 0:
-        num += 1
-
-    while num < upperlimit:
-        isprime = True
-        
-        for x in range(3, int(math.sqrt(num)) + 1, 2):
-            if num % x == 0: 
-                isprime = False
-                break
-        
-        if isprime:
-            yield num
-        
-        num += 2
+from sympy import isprime
 
 
-def main():
-    target = 50000000
-    prime_list2 = [x for x in prime_gen(2, int(target**0.5)+1)]
-    prime_list3 = [x for x in prime_gen(2, int(target**(1/3))+1)]                                    
-    prime_list4 = [x for x in prime_gen(2, int(target**(1/4))+1)] 
-    
-    print(len(set([x**2 + y**3 + z**4 for x in prime_list2 for y in prime_list3 \
-                   for z in prime_list4 if x**2 + y**3 + z**4 < target])))
+def prob_087(target=50_000_000):
+
+    limit = int(target**(1/2))
+
+    primes = [x for x in range(limit) if isprime(x)]
+
+    prime_power_set = set()
+
+    for i in primes:
+        for j in primes:
+            for k in primes:
+                num = i**2 + j**3 + k**4
+                if num > target:
+                    break
+                else:
+                    prime_power_set.update([num])
+
+    return len(prime_power_set)
 
 
 if __name__ == '__main__':
-    main()
-
-'''
-1097343
-'''
-
-time_end = time.perf_counter()
-print(f'Time taken = {time_end - time_start} sec')
-
-'''
-Time taken = 2.3967444 sec
-'''
+    start_time = time.perf_counter()
+    print(prob_087(target=50_000_000))
+    end_time = time.perf_counter()
+    print(f'Time taken = {end_time - start_time} sec')
 
 
-
-
-
+# 1097343
+# Time taken = 0.9645949000259861 sec
