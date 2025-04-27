b3fc6f2097bcd163360838240c2c0ed7f95096aa
Jim Yin <jimyin88@gmail.com>
2025-04-27 01:44:35 -0400

    Faster implementation

--- Problem037.py
+++ Problem037.py
@@ -3,59 +3,73 @@
 # @author: Jim Yin
 
 
-# The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, 
-# and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+# The number 3797 has an interesting property. Being prime itself,
+# it is possible to continuously remove digits from left to right,
+# and remain prime at each stage: 3797, 797, 97, and 7. Similarly, we can work from right to left: 3797, 379, 37, and 3.
 #
 # Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
 #
 # NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
 
-import time 
-import math
+import time
 from sympy import isprime
 
 
-def prime_gen():
-    yield 2
-    yield 3
-    num = 5
-    
-    while True:
-        isprime = True
-        
-        for x in range(3, int(math.sqrt(num) + 1), 2):
-            if num % x == 0: 
-                isprime = False
-                break
-        
-        if isprime:
+def sieve_of_eratosthenes(limit):
+    """Generate prime numbers up to a given limit using the Sieve of Eratosthenes."""
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
+
+    for start in range(2, int(limit**0.5) + 1):
+        if sieve[start]:
+            for multiple in range(start*start, limit + 1, start):
+                sieve[multiple] = False
+
+    for num in range(limit + 1):
+        if sieve[num]:
             yield num
-        
-        num += 2
 
 
 def trunctable(num):
+
     if len(str(num)) == 1:
         return False
 
-    for t in range(len(str(num))-1):
-        if not isprime(int(str(num)[t+1:])):
-            return False 
-    
-    for t in range(len(str(num))-1):
-        if not isprime(int(str(num)[0:-(t+1)])):
-            return False 
-    
+    for t in range(len(str(num)) - 1):
+        if not isprime(int(str(num)[t + 1:])):
+            return False
+
+    for t in range(len(str(num)) - 1):
+        if not isprime(int(str(num)[0:-(t + 1)])):
+            return False
+
     return True
+
+
+def prob_037(n_prime=11):
+
+    counter = 0
+    result = 0
+    primes = sieve_of_eratosthenes(10**6)
+
+    prime = next(primes)
+
+    while counter < n_prime:
+        if trunctable(prime):
+            counter += 1
+            result += prime
+        prime = next(primes)
+
+    return result
 
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    print(sum([i for i in range(1000000) if isprime(i) and trunctable(i)]))
+    print(prob_037(n_prime=11))
     end_time = time.perf_counter()
     print(f'Time taken = {end_time - start_time} sec')
-    
-    
+
+
 # 748317
-# Time taken = 2.6625965000000003 sec
+# Time taken = 0.35166209998715203 sec
