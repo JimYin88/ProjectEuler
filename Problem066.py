020e627cac1410837b2686a720e5ca7080e1a3c9
Jim Yin <jimyin88@gmail.com>
2025-04-27 01:32:54 -0400

    Date changed

--- Problem066.py
+++ Problem066.py
@@ -1,4 +1,4 @@
-# Created on April 19, 2025
+# Created on April 20, 2025
 #
 # @author: Jim Yin
 
@@ -7,7 +7,7 @@ import time
 from sympy.solvers.diophantine.diophantine import diop_DN
 
 
-def prob_066(limit = 1000):
+def prob_066(limit=1000):
 
     squares = [i**2 for i in range(1, int(limit**(1/2)) + 1)]
     d_candidates = [i for i in range(1, limit + 1) if i not in squares]
