95cffed0dc4a15ac53315fb195fef5751682c462
Jim Yin <jimyin88@gmail.com>
2025-04-28 18:04:34 -0400

    New solution

--- Problem078.py
+++ Problem078.py
@@ -1,167 +1,54 @@
-'''
-Created on Jun 2, 2022
+# Created on Jun 2, 2022
+#
+# @author: Jim Yin
 
-@author: Jim Yin
-'''
-
-from functools import cache
 
 import time
-time_start = time.time()
 
-# @cache
-# def find_coin_partition(target, maxcoin, multiplier):
-#     if maxcoin == 0:
-#         return 0
-#     val_prev = {target: 1}
-#
-#     for i in range(maxcoin, maxcoin-1, -1):
-#         val_next = {}
-#         for val in val_prev:
-#             num_of_subtraction = 0
-#             while val - num_of_subtraction*i >= 0:
-#                 if (val - num_of_subtraction*i) not in val_next:
-#                     val_next[val - num_of_subtraction*i] = val_prev[val]
-#                 else:
-#                     val_next[val - num_of_subtraction*i] += val_prev[val]
-#
-#                 num_of_subtraction += 1
-#
-#         val_consolidate = {}
-#         for k in val_next:
-#             if k not in val_consolidate:
-#                 val_consolidate[k] = val_next[k]
-#             else:
-#                 val_consolidate[k] += val_next[k]
-#
-#         val_prev = val_consolidate
-#
-#     return sum([find_coin_partition(k, maxcoin - 1, multiplier * v) if k >= 1 else v for k, v in val_prev.items()])
-#
-# def main():
-#     i = 10
-#     while True:            
-#         if find_coin_partition(i, i, 1) % 10**3 == 0:
-#             return i 
-#         else:
-#             i += 1
-#
-# print(main())
-
-# def find_coin_partition(target):
-#     val_prev = {target: 1}
-#
-#     for i in range(target, 0, -1):
-#         val_next = {}
-#         for val in val_prev:
-#             num_of_subtraction = 0
-#             while val - num_of_subtraction*i >= 0:
-#                 if (val - num_of_subtraction*i) not in val_next:
-#                     val_next[val - num_of_subtraction*i] = val_prev[val]
-#                 else:
-#                     val_next[val - num_of_subtraction*i] += val_prev[val]
-#
-#                 num_of_subtraction += 1
-#
-#         val_consolidate = {}
-#         for k in val_next:
-#             if k not in val_consolidate:
-#                 val_consolidate[k] = val_next[k]
-#             else:
-#                 val_consolidate[k] += val_next[k]
-#
-#         val_prev = val_consolidate
-#
-#     return val_prev[0], val_prev
-#
-# def main():
-#     i = 10
-#     while True:            
-#         if find_coin_partition(i) % 10**4 == 0:
-#             return i 
-#         else:
-#             i += 1
-
-# print(main())
-
-# print(find_coin_partition(4))
+number_partitions_dict = {0: 1, 1: 1, 2: 2}
 
 
+def n_partitions(n):
+    
+    global number_partitions_dict
 
-def find_coin_partition(num_ways):
-    target = 2
+    if n < 0:
+        number_partitions_dict[n] = 0
+        return 0
+
+    if n in number_partitions_dict:
+        return number_partitions_dict[n]
+
+    sm = 0
+
+    for k in range(1, n + 1):
+        n1 = n - int(k * (3 * k - 1) / 2)
+        n2 = n - int(k * (3 * k + 1) / 2)
+        sm += (-1) ** (k + 1) * (n_partitions(n1) + n_partitions(n2))
+        if n1 <= 0:
+            break
+
+    number_partitions_dict[n] = sm
+    return sm
+
+
+def prob_078():
+
+    start = 2
     while True:
-        val_initial = {target: 1}
-        val_prev = val_initial
-
-        for i in range(target, 0, -1):
-            val_next = {}
-            for val in val_prev:
-                num_of_subtraction = 0
-                while val - num_of_subtraction*i >= 0:
-                    if (val - num_of_subtraction*i) not in val_next:
-                        val_next[val - num_of_subtraction*i] = val_prev[val]
-                    else:
-                        val_next[val - num_of_subtraction*i] += val_prev[val]
-
-                    num_of_subtraction += 1
-
-            val_consolidate = {}
-            for k in val_next:
-                if k not in val_consolidate:
-                    val_consolidate[k] = val_next[k]
-                else:
-                    val_consolidate[k] += val_next[k]
-
-            val_prev = val_consolidate
-
-        if 0 not in val_prev:
-            target += 1
-        elif val_prev[0] < num_ways:
-            target += 1
+        partitions = n_partitions(start)
+        if partitions % 1000000 == 0:
+            return start
         else:
-            return target
-
-print(find_coin_partition(10**4))
+            start += 1
 
 
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    print(prob_078())
+    end_time = time.perf_counter()
+    print(f'Time taken = {round(end_time - start_time, 4)} sec')
 
 
-# coin_start = 5
-# rem_initial = {coin_start: 1}
-# rem_prev = rem_initial
-#
-# for i in range(coin_start, 0, -1):
-#     rem_next = {}
-#     for rem in rem_prev:
-#         num_of_subtraction = 0
-#         while rem - num_of_subtraction*i >= 0:
-#             if (rem - num_of_subtraction*i) not in rem_next:
-#                 rem_next[rem - num_of_subtraction*i] = rem_prev[rem]
-#             else:
-#                 rem_next[rem - num_of_subtraction*i] += rem_prev[rem]
-#
-#             num_of_subtraction += 1
-#
-#     rem_consolidate = {}
-#     for k in rem_next:
-#         if k not in rem_consolidate:
-#             rem_consolidate[k] = rem_next[k]
-#         else:
-#             rem_consolidate[k] += rem_next[k]
-#
-#     rem_prev = rem_consolidate
-#
-# print(rem_prev[0])            
-                
-'''
-190569291
-'''
-
-time_end = time.time()
-print("time taken", time_end-time_start)
-
-'''
-time taken 0.01896977424621582
-'''
-        
+# 55374
+# Time taken = 8.707 sec
